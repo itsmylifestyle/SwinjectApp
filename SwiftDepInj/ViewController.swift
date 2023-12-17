7d5086b6d304d27ad314583bbb0a1a04711173af
itsmylifestyle <hahaii00@mail.ru>
2023-12-17 20:05:14 +0600

    Initial Commit

---
+++ SwiftDepInj/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftDepInj
+//
+//  Created by Айбек on 17.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
